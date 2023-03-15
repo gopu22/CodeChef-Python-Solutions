970cb2d35c23af8951b93f733d4b6e4b87ce600a
Govardhan Reddy B <govardhanbs2000@gmail.com>
2023-03-15 21:09:29 +0530

    15-Mar-2023

---
+++ Dominant_Army.py
@@ -0,0 +1,9 @@
+# cook your dish here
+
+for i in range(int(input())):
+    a,b,c=map(int, input().split())
+    if (a>b+c) or (b>a+c) or (c>a+b):
+        print("Yes")
+    else:
+        print("No")
+

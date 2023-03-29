7d8a53a42b7fdf5bbfa04000a867f1b149f3c006
Govardhan Reddy B <govardhanbs2000@gmail.com>
2023-03-29 22:46:04 +0530

    29-Mar-2023

---
+++ Solutions!/Is_the_Score_Consistent.py
@@ -0,0 +1,10 @@
+# cook your dish here
+
+for i in range(int(input())):
+    a,b=map(int,input().split())
+    c,d=map(int,input().split())
+    if a<=c and b<=d:
+        print("possible")
+    else:
+        print("impossible")
+

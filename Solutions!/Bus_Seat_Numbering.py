ae75a62fd0f830e4b048b2fbab0cec331c46e257
Govardhan Reddy B <govardhanbs2000@gmail.com>
2023-05-29 22:36:06 +0530

    29-May-2023

---
+++ Solutions!/Bus_Seat_Numbering.py
@@ -0,0 +1,15 @@
+# cook your dish here
+
+for i in range(int(input())):
+    n=int(input())
+    if n<16:
+        if n<11:
+            print("Lower Double")
+        else:
+            print("Lower Single")
+    else:
+        if n<26:
+            print("Upper Double")
+        else:
+            print("Upper Single")
+

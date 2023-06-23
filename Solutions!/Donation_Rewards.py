1fa6f8ba253afa11a0878b842d5caae3dfb97fc3
Govardhan Reddy B <govardhanbs2000@gmail.com>
2023-06-23 23:18:22 +0530

    23-Jun-2023

---
+++ Solutions!/Donation_Rewards.py
@@ -0,0 +1,11 @@
+# cook your dish here
+
+for i in range(int(input())):
+    x=int(input())
+    if x<=3:
+        print("BRONZE")
+    elif 3<x<=6:
+        print("SILVER")
+    else:
+        print("GOLD")
+

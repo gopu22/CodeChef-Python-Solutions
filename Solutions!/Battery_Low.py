21a11ebb7c6a308023158e1c379e1911c0d84d9d
Govardhan Reddy B <govardhanbs2000@gmail.com>
2023-07-27 23:57:42 +0530

    27-Jul-2023

--- Solutions!/Battery_Low.py
+++ Solutions!/Battery_Low.py
@@ -1,9 +1,9 @@
 # cook your dish here
 
-for i in range(int(input())):
-    x=int(input())
-    if x<=15:
-        print("yes")
+for i in range(int(input())):  # for() loop
+    x=int(input())  # takes user5's input
+    if x<=15:  # if condition
+        print("yes")  # printing values
     else:
         print("no")
-
+# final output

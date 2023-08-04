315ba33066746cf70038ef5ca9e75585628b8bbf
Govardhan Reddy B <govardhanbs2000@gmail.com>
2023-08-04 23:40:17 +0530

    04-Aug-2023

---
+++ Solutions!/Indivisible.py
@@ -0,0 +1,9 @@
+# cook your dish here
+
+for t in range(int(input())):
+    a, b, c = map(int, input().split())
+    for k in range(1, 100):
+        if a%k != 0 and b%k != 0 and c%k != 0:
+            print(k)
+            break
+

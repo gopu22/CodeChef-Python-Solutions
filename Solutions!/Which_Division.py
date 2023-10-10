ecd596000d0174eff82260702f24c8604cd4cac6
Govardhan Reddy B <govardhanbs2000@gmail.com>
2023-10-10 23:12:28 +0530

    10-Oct-2023

---
+++ Solutions!/Which_Division.py
@@ -0,0 +1,11 @@
+# cook your dish here
+
+for _ in range(int(input())):
+    R=int(input())
+    if 2000<=R:
+        print(1)
+    elif 1600<=R<2000:
+        print(2)
+    else:
+        print(3)
+

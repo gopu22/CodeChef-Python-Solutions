cdbdbef1b4b648f3ec1659e7e71a321227d8a409
Govardhan Reddy B <govardhanbs2000@gmail.com>
2023-11-17 14:59:28 +0530

    17-Nov-2023

---
+++ Solutions!/Rivalry.py
@@ -0,0 +1,11 @@
+# cook your dish here
+
+r1,r2=map(int,input().split())
+d1,d2=map(int,input().split())
+a=((r1)+(d1))
+b=((r2)+(d2))
+if a>b:
+    print('Dominater')
+else:
+    print('Everule')
+
